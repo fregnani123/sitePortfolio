c406e8c1e5795859d0db23728c84bc458dcaf4e2
fregnani123 <fabianofregnani@hotmail.com>
2024-01-07 10:45:48 -0300

    implementando modo escuro

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,6 +1,9 @@
 import '../components/body.css';
 import Header from '../components/header';
-import fabianoIMG from '../assets/img/fabianoIMG.png';
+import fabianoIMG from '../assets/img/fotoComFundo.png';
+import git from '../assets/img/git.png'
+import linkedin from '../assets/img/logotipo-do-linkedin.png'
+import whatsap from '../assets/img/whatsapp.png'
 
 
 function body() {
@@ -9,7 +12,7 @@ function body() {
         <div className='main-container'>
             <h1 className='logo'>Fabiano Fregnani</h1>
             <Header />
-            <div className='divBodyPartOne'></div><div className='containerPerfil' ><img className='fabianoImg' src={fabianoIMG}/></div><div className='divBodyPartTwo'></div>
+            <div className='divBodyPartOne'></div><div className='containerPerfil'><img className='fabianoImg' src={fabianoIMG} /></div><div className='divBodyPartTwo'><section className='sectionNome'><h1 className='nome'>Fabiano Fregnani</h1><p className='subTitulo'>Full-Stack Developer</p><button className='CV'>Download CV</button></section> <section ><ul className='midiaSocial'><li><img className='git' src={git}/></li><li><img className='linkedin' src={linkedin}/></li> <li><img className='whatsap' src={whatsap} alt="" /></li></ul></section></div>
         </div>
     )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,17 @@
 import '../components/header.css';
 import { Link } from 'react-router-dom';
+import lua from '../assets/img/forma-de-meia-lua.png'
+import sol from '../assets/img/ensolarado.png'
+import { useState } from 'react';
 
 function header() {
 
+    const [active, setClaroEscuro] = useState(false);
+    
+    const toggleCor = () => {
+        setClaroEscuro(!active)
+    }
+    
     const listaMenu = [
         { id: '1', texto: 'Home', to: '/' },
         { id: '2', texto: 'About Me', to: '/' },
@@ -14,7 +23,9 @@ function header() {
         <ul className='menu-navBar'>
             {listaMenu.map((lista) => ( <Link className='Link' to={lista.to}>
                 <li className='li-Menu' id={lista.idClass} key={lista.id}>{lista.texto}</li></Link>
-         ))} 
+            ))} 
+            <li><img className= 'claro' onClick={toggleCor} src={lua}/></li>
+            <li><img id='escuro' src={sol} onClick={toggleCor}/></li>
         </ul>
     </nav>)
 
