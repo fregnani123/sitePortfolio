023cbfdbe97ff8ea681d4877f7c6bfc748aa7f03
fregnani123 <fabianofregnani@hotmail.com>
2024-01-12 18:16:46 -0300

    melhorias css

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,37 +1,47 @@
 import '../components/body.css';
 import Header from '../components/header';
 import fabianoIMG from '../assets/img/fotoComFundo.png';
+import lua from '../assets/img/lua.png';
 import git from '../assets/img/git.png'
 import linkedin from '../assets/img/logotipo-do-linkedin.png'
-import whatsap from '../assets/img/whatsapp.png'
+import whatsap from '../assets/img/whatsapp.png';
+import arvore from '../assets/img/arvore.png';
+import arvoreSeca from '../assets/img/arvoreSeca.png';
+// import nave from '../assets/img/space.gif'
+import passaros from '../assets/img/passaros.gif'
 import { useState } from 'react';
-
 
 function Body() {
     const [active, setClaroEscuro] = useState(false);
-    
-
     const toggleCor = () => {
         setClaroEscuro(!active);
     }
     
     return (
         <div className='main-container'>
-            <div className={active ? 'body-ContainerEscura':'body-Container'}>
+            <div className={active ? 'body-ContainerEscura' : 'body-Container'}>
+                {/* <img className={active ? 'naveActive' : 'nave'} src={nave} alt="" /> */}
+                <img className={active ? 'passaros1':'passarosActive1'} src={passaros} alt="" />
+                <img className={active ? 'passaros': 'passarosActive2'} src={passaros} alt="" />
+                <img className={active ? 'passaros' : 'passarosActive3'}src={passaros} alt="" />
+                <img className={active ? 'arvore' : 'arvoreActive'} src={arvore} alt="" />
+                <img className={active ? 'arvoreSecaActive' : 'arvoreSeca'} src={arvoreSeca} alt="" />
                 <div className='divHeader'><h1 className='logo'>Fabiano Fregnani</h1>
                     <Header {...{ toggleCor, active }} /></div> 
                 <div className='divBody'>
-                    <img className='fabianoImg' src={fabianoIMG} />
+                    <img className={active ? 'fabianoActive' : 'fabianoImg' } src={fabianoIMG}/>
+                    <img className={active ? 'luaActive' : 'lua'} src={lua}/>
                     <section className='perfil'>
-                        <p className='hello'>Hello, I'm </p>
-                        <h1 className='nome'>Fabiano Fregnani</h1>
-                        <p className='subTitulo'>Full-Stack Developer</p>
-                        <button className='CV'>Download CV</button>
+                        <p className={active ? 'helloActive':'hello'}>Hello, I'm </p>
+                        <h1 className={active ? 'nomeActive' : 'nome'}>Fabiano Fregnani</h1>
+                        <p className={active ? 'subtituloActive' : 'subtitulo'}>Full-Stack Developer</p>
+                        {/* <button className='CV'>Download CV</button> */}
                         <ul className='midiaSocial'>
                             <li><img className='git' src={git} /></li>
                             <li><img className='linkedin' src={linkedin} /></li>
                             <li><img className='whatsap' src={whatsap} alt="" /></li>
                         </ul></section>
+                    
                         </div>     
                    </div>
             </div>
