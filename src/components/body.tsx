1ab3e0d9d31a318b7bf92854e40aefc284955ee1
fregnani123 <fabianofregnani@hotmail.com>
2024-01-14 15:35:50 -0300

    começar do zero

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -2,17 +2,13 @@
 
 import '../components/body.css';
 
-
-
 function body(){
     return (<body><div className='container'>
-        <div className="item1">Elemento 1</div>
-        <div className="item2">Elemento 2</div>
-        <div className="item3">Elemento 3</div>
-        <div className="item4">Elemento 4</div>
-        <div className="item5">Elemento 5</div>
-        <div className="item6">Elemento 6</div></div>
-    </body>)
+        
+        </div>
+       
+        </body>)
+
 };
 
 export default body;
