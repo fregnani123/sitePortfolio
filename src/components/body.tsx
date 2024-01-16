b1f5aafebe6eb16cf71ed8e2bbfac8e700f07917
fregnani123 <fabianofregnani@hotmail.com>
2024-01-16 20:53:39 -0300

    melhorias css

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,35 +1,54 @@
 import { useState } from 'react';
 import '../components/body.css';
 import bg from '../assets/img/cityDia.png'
+import FabianoFoto from '../assets/img/fabiano.png'
 import bgNoite from '../assets/img/cityNoite.png'
 import luaNoite from '../assets/img/lua.png';
 import arvore from '../assets/img/arvore1.png';
 import arvoreNoite from '../assets/img/arvoreNoite.png';
 import outdoor from '../assets/img/outdorBg.png';
 import passaros from '../assets/img/passaros.gif';
-import gato from '../assets/img/outdorBg.png';
+import gato from '../assets/img/gato.gif';
+import bancoDia from '../assets/img/bancoPraça.png';
+import Space from '../assets/img/spaceAlien.gif';
+import sombra from '../assets/img/loop-104569-720p--unscreen.gif';
+
+
 import Header from './header';
 
 function body() {
     
     const [active, setActive] = useState(false);
+    const [interagir, setInteragir] = useState(false);
 
     function toggleCor() {
             setActive(!active)
     }
 
+    function toggleInteragir(){
+            setInteragir(!interagir)
+    }
+
     return (<body><div className='Header'><Header {...{ toggleCor, active }} /></div><div className={active ? 'container' : 'containerNoite'}>
         <div className='divImg'>
-            <span className='logoOutdoor'>🅵🅰🅱🅸🅰🅽🅾 🅵🆁🅴🅶🅽🅰🅽🅸 <br />Fᴜʟʟ Sᴛᴀᴄᴋ Dᴇᴠᴇʟᴏᴘᴇʀ</span>
+       
+            <ul className='logoOutdoor'>
+                <li className='liOutdoor'>🅵<span className='my-letterOutdoor'>A</span><span className='my-letterOutdoor'>B</span>🅸<span className='my-letterOutdoor'>A</span>🅽<span className='my-letterOutdoor'>O</span> 🅵🆁🅴🅶🅽<span className='my-letterOutdoor'>A</span>🅽🅸 <br />Fᴜʟʟ Sᴛᴀᴄᴋ Dᴇᴠᴇʟᴏᴘᴇʀ</li>
+                <li><img className='sombra' src={sombra} alt=""/></li>
+            </ul>
+        <img className='fotoPerfil' src={FabianoFoto}></img>
         <img className={active ? 'luaNone' : 'luaNoite'} src={luaNoite} alt="" />
-            <img className={active ? 'cityNone':'city'} src={bg} alt="" />
-            <img className={active ?'cityNoite':'cityNoiteNone' } src={bgNoite} alt="" />
+        <img className={active ? 'cityNone':'city'} src={bg} alt="" />
+        <img className={active ?'cityNoite':'cityNoiteNone' } src={bgNoite} alt="" />
         <img className={active ? 'arvoreDia':'arvoreDiaNone' } src={arvore} alt="" />
         <img className={active ? 'arvoreNoiteNone':'arvoreNoite'} src={arvoreNoite} alt="" />
         <img className={active ? 'outdoorDia':'outdoorNoite' } src={outdoor} alt="" />
         <img className={active ? 'passaroDia':'passorDiaNone' } src={passaros} alt="" />
         <img className={active ? 'passaroDia1':'passorDiaNone1' } src={passaros} alt="" />
-            <img className={active ? 'passoroNoiteNone':'passaroNoite' } src={passaros} alt="" />
+        <img className='bancoDia' src={bancoDia} alt=""/>
+        <img className={active ? 'passoroNoiteNone':'passaroNoite' } src={passaros} alt="" />
+        <img className={active ? 'gatoNoiteNone':'gatoNoite' } src={gato} alt="" />
+        <img className={active ? 'space':'spaceNoite' } src={Space} alt="" />
         </div>
         </div>
        
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 import '../components/header.css';
-import lua from '../assets/img/forma-de-meia-lua.png';
-import sol from '../assets/img/ensolarado.png';
+import lua from '../assets/img/ensolarado.png';
+import sol from '../assets/img/forma-de-meia-lua.png';
+import linkedin from '../assets/img/logotipo-do-linkedin.png';
+import git from '../assets/img/git.png';
+import whatsap from '../assets/img/whatsapp.png';
+import { useState } from 'react';
 
 interface HeaderProps {
     toggleCor: () => void;
@@ -13,23 +17,44 @@ function Header({ toggleCor, active}: HeaderProps) {
         { id: '1', texto: 'Home', to: '/' },
         { id: '2', texto: 'About Me', to: '/' },
         { id: '3', texto: 'Portfolio', to: '/' },
-        { id: '4', texto: 'Contact Me', to: '/', idClass: 'contact' },
     ];
+    const listaContato = [
+        { id: '4', texto: 'LinkedIn', icon: linkedin, link: '', className:'linkedin', },
+        { id: '5', texto: 'GitHub', icon: git, link: 'https://github.com/fregnani123', className: 'git', },
+        { id: '6', texto: 'WhatsApp', icon: whatsap, link: 'https://api.whatsapp.com/send?phone=5548996607600&text=', className: 'whatsap', },
+    ];
+
+    const [menuList, setMenuList] = useState(false)
+    
+    const listaActive = () => {
+        setMenuList(!menuList)
+    }
 
     const menuClass = active ? 'menu-mobile' : 'menu-navBarEscuro';
 
     return (
         <nav className={menuClass}>
             <ul className='menu-navBar'>
-                <li className='logo'>🅵🆁🅴🅶🅽🅰🅽🅸</li>
-                {listaMenu.map(({ id, texto, to, idClass }) => (
+                <li className='logo'>🅵🆁🅴🅶🅽<span className='my-letter'>A</span>🅽🅸</li>
+                {listaMenu.map(({ id, texto, to}) => (
                     <Link className='Link' to={to} key={id}>
-                        <li className={active ? 'li-Menu' : 'li-MenuNoite'}id={idClass}>{texto}</li>
+                        <li className={active ? 'li-Menu' : 'li-MenuNoite'}>{texto}</li>
                     </Link>
-                ))}
+                ))}<li>
+                    <ul className={menuList?'menu-contacts':'menu-contactsActive'}>
+                    <li onClick={listaActive} className='contact'>Contact</li>
+                    {listaContato.map(({ id, texto, icon, link, className }) => (
+                        <li key={id}>
+                            <a href={link} target='_blank' rel='noopener noreferrer'>
+                                <img className={className} src={icon} alt={texto} /><span className='textoMenu'>{texto}</span>
+                            </a>
+                        </li>
+                    ))}
+                </ul></li>
                 <li><img className={active ? 'active' : 'claro'} onClick={toggleCor} src={lua} alt='Lua' /></li>
                 <li><img id='escuro' className={active ? 'claro' : 'active'} onClick={toggleCor} src={sol} alt='Sol' /></li>
             </ul>
+           
         </nav>
     );
 }
